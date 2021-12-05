9c0d264647f436c5aa30862c729f9c9d29e884c8
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2021-12-04 21:20:16 -0300

    alteração

---
+++ Java/Aula005.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class Aula005 {
+    public static void main(String[] args) {
+         /*var login = "teste";
+        var password = "12345";
+
+        System.out.println(login + " " + password);*/
+
+        //Tipos de dados
+        // variáveis -> tipo texto (String)
+        // variáveis -> tipo número (int)
+        // variáveis -> tipo fracionado (double)
+        // varivaies -> tipo condição (booblean)
+
+        //Atualmente Java aceita variaveis, mas antigamente era necessário colocar o tipo de dado antes de declarar
+        String name = "Rubens";
+        int num = 100;
+        Double numDouble = 10.5;
+        Boolean cond = true;
+        System.out.println(name);
+        System.out.println(num);
+        System.out.println(numDouble);
+        System.out.println(cond);
+    }
+}
