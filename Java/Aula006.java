7874a8551017c23615b68ab763f87d2faec08e98
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2021-12-19 18:02:36 -0300

    19/12/2021

---
+++ Java/Aula006.java
@@ -0,0 +1,13 @@
+public class Aula006 {
+    public static void main(String[] args) {
+        // 2 tipos de erros
+        // erro por compilação
+        String text = "ola mundo";
+        System.out.println(text);
+
+        // erro de execução
+        int x = 10;
+        int y = 20;
+        System.out.println(x / y);
+    }
+}
---
+++ Java/Aula007.java
@@ -0,0 +1,14 @@
+public class Aula007 {
+    public static void main(String[] args) {
+        sum(10, 15); // argumentos
+    }
+    static void sum(int x, int y){
+        // static void (declaração padrão)
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+
+    }
+}
