2abafef593c34020c366f2d8e54684c8900681dc
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2021-12-21 12:22:36 -0300

    Aula 008 Java

---
+++ Java/Aula008.java
@@ -0,0 +1,6 @@
+public class Aula008 {
+    public static void main(String[] args) {
+        //String[] args é um conjunto de strings
+        System.out.println("Olá, " + args[0] + " " + args[1]);
+    }
+}
