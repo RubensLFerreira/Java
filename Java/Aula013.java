8c271633352fe09cb38070ec3bb1dfe6a7d1b739
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2021-12-29 18:13:58 -0300

    Aula 013 Java

---
+++ Java/Aula013.java
@@ -0,0 +1,31 @@
+public class Aula013 {
+    public static void main(String[] args) {
+        int semana = 2;
+
+        switch(semana){
+            case 1:
+                System.out.println("Domingo, final de semana.");
+                break;
+            case 2:
+                System.out.println("Segunda, final de semana.");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break ;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("[ERRO]");
+        }
+    }
+}
