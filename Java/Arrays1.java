f65776509655a40f0e04c0233708c8a41ba9cd6e
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-01-08 23:10:02 -0300

    Controle de fluxo

---
+++ Java/Arrays1.java
@@ -0,0 +1,12 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        idade[0] = 3;
+        idade[1] = 5;
+        idade[2] = 8;
+
+        System.out.println(idade[0]);
+        System.out.println(idade[1]);
+        System.out.println(idade[2]);
+    }
+}
